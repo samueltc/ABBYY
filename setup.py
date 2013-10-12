51e2557e65dc02c5e5e5fe488dad0477dea5be37
Samuel Cossette <samueltc@gmail.com>
2013-10-12 19:43:15 -0400

    package requirements

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='ABBYY',
+    version='0.2',
+    author='Samuel Cossette',
+    author_email='samueltc@gmail.com',
+    packages=['ABBYY',],
+    install_requires = [
+        'requests>=1.2.3', 
+        'lxml>=3.2.1'
+        ],
+    url='https://github.com/samueltc/ABBYY',
+    license='LICENSE',
+    description='ABBYY Cloud OCR API Wrapper.',
+    long_description=open('README.md').read(),
+)
