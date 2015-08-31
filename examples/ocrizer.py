c1e04a306805783c06958087ca3e687b842bb4fa
Ethan McCreadie <ethanmcc@gmail.com>
2015-08-31 10:44:56 -0400

    Add Python 3 support

--- examples/ocrizer.py
+++ examples/ocrizer.py
@@ -13,12 +13,12 @@ if __name__=="__main__":
 	parser.add_argument('--inputFilename', help='', required=True)
 	args = parser.parse_args()
 
-	if 'ABBYY_APPLICATION_ID' in os.environ.keys():
+	if 'ABBYY_APPLICATION_ID' in list(os.environ.keys()):
 		application_id = os.environ['ABBYY_APPLICATION_ID']
 	else:
 		application_id = args.application_id
 
-	if 'ABBYY_PASSWORD' in os.environ.keys():
+	if 'ABBYY_PASSWORD' in list(os.environ.keys()):
 		password = os.environ['ABBYY_PASSWORD']
 	else:
 		password = args.password
@@ -26,12 +26,12 @@ if __name__=="__main__":
 	ocr_engine = CloudOCR(application_id, password)
 
 	api_parameters = ['language', 'textType', 'exportFormat', 'pdfPassword']
-	parameters = dict(filter(lambda x: x[0] in api_parameters and x[1] is not None, args._get_kwargs()))
+	parameters = dict([x for x in args._get_kwargs() if x[0] in api_parameters and x[1] is not None])
 	
 	input_file = open(args.inputFilename, 'rb')
 	post_file = {input_file.name: input_file}
 	result = ocr_engine.process_and_download(post_file, **parameters)
-	for format, content in result.iteritems():
+	for format, content in result.items():
 		output_filename = '{name}.{extension}'.format(name='.'.join(input_file.name.split('.')[:-1]), extension=format)
 		with open(output_filename, 'wb') as output_file:
 			output_file.write(content.read())
